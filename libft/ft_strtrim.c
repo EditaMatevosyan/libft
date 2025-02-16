24a54c5e46bb15c5e33c8dc2f0896229345dd49f
EditaMatevosyan <125731320+EditaMatevosyan@users.noreply.github.com>
2025-02-16 18:59:37 +0400

    Update ft_strtrim.c

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,14 @@
 /*   By: edmatevo <edmatevo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:11:45 by edmatevo          #+#    #+#             */
-/*   Updated: 2025/02/15 16:34:40 by edmatevo         ###   ########.fr       */
+/*   Updated: 2025/02/16 18:16:29 by edmatevo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-int	check(char c, char const *set)
+int	check(char c, unsigned char *set)
 {
 	int	i;
 
@@ -28,26 +29,24 @@ int	check(char c, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char    *str;
-	int     i;
-	int     start;
-	int     end;
+	char	*str;
+	int		i;
+	int		start;
+	int		end;
 
 	if (!s1 || !set)
 		return (NULL);
-	if (*s1 == '\0')
-		return (ft_strdup(""));
 	start = 0;
-	while (s1[start] && check(s1[start], set))
-		start++;
 	end = ft_strlen(s1) - 1;
-	while (check(s1[end], set) && end >= start)
+	while (s1[start] && check(s1[start], (unsigned char *)set))
+		start++;
+	while (check(s1[end], (unsigned char *)set) && end >= start)
 		end--;
 	str = (char *)malloc(end - start + 2);
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (start + i <= end)
+	while (i < end - start + 1)
 	{
 		str[i] = s1[start + i];
 		i++;
